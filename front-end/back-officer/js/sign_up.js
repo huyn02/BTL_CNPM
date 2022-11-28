00fd260162ae0ae9fc5736fa8f2ea2e9ae4e127c
Dat <huynhtandat3636@gmail.com>
2022-11-28 16:05:43 +0700

    front_end

---
+++ front-end/back-officer/js/sign_up.js
@@ -0,0 +1,14 @@
+function show_password() {
+    var password = document.getElementById("password");
+
+    if (password.type == "password") {
+        password.type = "text";
+    }
+
+    else if (password.type == "text") {
+        password.type = "password";
+    }
+}
+function notice() {
+    alert("Coming Soon !");
+}
