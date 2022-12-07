aadc5440126a1634da1b6e38afffed7cf69528bc
huynhat0207 <huynhat0207@gmail.com>
2022-12-07 19:34:51 +0700

    update create profile

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function selected(id) {
     var tasks_m = document.getElementById('tasks_m');
     var messages_m = document.getElementById('messages_m');
     var profiles_m = document.getElementById('profiles_m');
+    var create_profile_m = document.getElementById('create_profile_m');
 
     var tasks = document.getElementById('tasks');
     var messages = document.getElementById('messages');
@@ -28,6 +29,7 @@ function selected(id) {
         tasks_m.style.display = 'block';
         messages_m.style.display = 'none';
         profiles_m.style.display = 'none';
+        create_profile_m.style.display = "none";
         id_item.className = 'selected';
         messages.className = 'not-selected';
         profiles.className = 'not-selected';
@@ -39,6 +41,7 @@ function selected(id) {
         messages_m.style.display = 'block';
         tasks_m.style.display = 'none';
         profiles_m.style.display = 'none';
+        create_profile_m.style.display = "none";
         id_item.className = 'selected';
         tasks.className = 'not-selected';
         profiles.className = 'not-selected';
@@ -50,7 +53,20 @@ function selected(id) {
         profiles_m.style.display = 'block';
         messages_m.style.display = 'none';
         tasks_m.style.display = 'none';
+        create_profile_m.style.display = "none";
         id_item.className = 'selected';
+        messages.className = 'not-selected';
+        tasks.className = 'not-selected';
+    }
+    else if(id == 'create_profile'){
+        tttasks.className = 'not-show';
+        ttmess.className = 'not-show';
+        ttpro.className = 'text16918 show';
+        create_profile_m.style.display = "block";
+        profiles_m.style.display = 'none';
+        messages_m.style.display = 'none';
+        tasks_m.style.display = 'none';
+        profiles.className = 'selected';
         messages.className = 'not-selected';
         tasks.className = 'not-selected';
     }
